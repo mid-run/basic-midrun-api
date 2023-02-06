78760f52ef4b2985062feae07328e7a431a00ce2
felix <you@example.com>
2023-02-06 11:11:13 -0500

    ⚡

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 var cors = require("cors");
 const midrun = require("midrun-node");
-
 const app = express();
 app.use(cors());
-
 const port = process.env.PORT || 3000;
-
 const routes = require("./routes");
 
 app.use("/", midrun.router([routes], {}));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`⚡ Example Midrun API listening on port ${port}`);
 });
